22ac2ad62afebe0d72c897c2823e0b3268030d9e
hisao5232 <hisao5232@gmail.com>
2026-09-14 01:12:27 +0000

    fix next.config.ts

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: 'export',
+  // 画像の最適化サーバー（Next.js標準）を使わない設定（静的エクスポートでエラーを出さないため）
+  images: {
+    unoptimized: true,
+  },
 };
 
 export default nextConfig;
+
